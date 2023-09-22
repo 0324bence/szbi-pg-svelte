b7b56998a127686050119a4199c323ccaf1da891
0324bence <d.bence0324@gmail.com>
2023-09-22 12:13:55 +0200

    Add text of contacts page

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,4 +1,32 @@
 <svelte:head>
     <title>Elérhetőségek</title>
 </svelte:head>
-<h1>Teszt</h1>
+
+<h2>Kiskunfélegyházi Szent Benedek PG Két Tanítási Nyelvű Technikum és Kollégium</h2>
+<p>OM: 203365</p>
+<p>6100 Kiskunfélegyháza, Kossuth u. 24.</p>
+<p>
+    <a href="https://szbi-pg.hu/images/phocadownload/Kozzeteteli/Alap%C3%ADt%C3%B3%20Okirat%20technikum.pdf"
+        >Intézményünk alapító okirata</a
+    >, annak kelte: 2020. május 21
+</p>
+<p>
+    <a
+        href="https://szbi-pg.hu/images/phocadownload/Kozzeteteli/Joger%C5%91s%20m%C5%B1k%C3%B6d%C3%A9si%20enged%C3%A9ly.pdf"
+        >Intézményünk működési engedélye</a
+    >
+</p>
+<p>
+    <a
+        href="https://szbi-pg.hu/images/phocadownload/Kozzeteteli/nyilv%C3%A1ntart%C3%A1sba%20v%C3%A9teli%20Kiskunfelegyhazi_Szent_Benedek.pdf"
+        >Intézményünk nyilvántartásba vételi határozat</a
+    >ának száma, kelte, az engedélyező hatóság: NSZFH/NSZFH/008967-50/2020, 2020. június 30. Nemzeti Szakképzési és
+    Felnőttképzési Hivatal
+</p>
+<a href="mailto:titkarsag@szbi-pg.hu">titkarsag@szbi-pg.hu</a>
+<p>+36 76 462 332</p>
+<p>Mellékek:</p>
+<p>11 - titkárság</p>
+<p>14 - tanári</p>
+<p>15 - kollégium</p>
+<p>16 - könyvtár</p>

9e969e01d884cdead03f14d134c96553c9beb395
0324bence <bence20050324@gmail.com>
2023-09-24 11:43:40 +0200

    Create html for image links

---
+++ src/components/ImageLink.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let image: string;
+    export let title: string;
+    export let subtitle: string;
+    export let link: string;
+</script>
+
+<a class="image-link" href={link}>
+    <img src={image} alt="{title} image" />
+    <div class="texts">
+        <p class="subtitle">{subtitle}</p>
+        <div class="title-box">
+            <p class="title">{title}</p>
+        </div>
+    </div>
+</a>

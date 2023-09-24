c830ca76f125e099ac7d9631c2edf935f2c6f059
0324bence <bence20050324@gmail.com>
2023-09-24 11:43:54 +0200

    Add styles to image links

--- src/components/ImageLink.svelte
+++ src/components/ImageLink.svelte
@@ -14,3 +14,73 @@
         </div>
     </div>
 </a>
+
+<style lang="scss">
+    .image-link {
+        color: white;
+        display: block;
+        width: 100%;
+        position: relative;
+        height: fit-content;
+        padding: 0;
+        line-height: 0;
+
+        img {
+            width: 100%;
+        }
+
+        .texts {
+            transition: color 400ms ease, background-color 400ms ease;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            overflow: hidden;
+
+            &:hover {
+                background-color: rgba(0, 0, 0, 0.5);
+
+                .title-box {
+                    transform: translateY(100%);
+                }
+
+                .subtitle {
+                    transform: translateY(0);
+                    opacity: 1;
+                }
+            }
+
+            p {
+                text-transform: uppercase;
+                font-family: Open Sans, sans-serif;
+                line-height: normal;
+                transition: opacity 0.2s, transform 0.35s;
+            }
+
+            .subtitle {
+                font-size: 0.9rem;
+                margin: 1em;
+                margin-top: 1.2em;
+                color: rgba(255, 255, 255, 0.8);
+                transform: translateY(100%);
+                opacity: 0;
+            }
+
+            .title-box {
+                width: 100%;
+                height: 15%;
+                background-color: rgba(255, 255, 255, 0.7);
+                color: black;
+                display: flex;
+                align-items: center;
+                padding: 10px;
+                font-weight: 600;
+                transition: transform 0.35s;
+            }
+        }
+    }
+</style>

264e0cd3855b378df33d42324f32f054bf46da97
timre13 <itorteli13@gmail.com>
2023-09-26 19:30:14 +0200

    Add script to fetch colleague list

---
+++ scripts/get_colleague_list.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup, PageElement
+import json
+
+url = "https://szbi-pg.hu/index.php/hu/munkatarsaink"
+
+soup = BeautifulSoup(requests.get(url).content.decode(), features="lxml")
+
+sections = [x for x in soup.find("div", id="agmedia_responsibilities").find("div").findAll("div")]
+
+output = []
+for section in sections:
+    if section == None:
+        continue
+    header = section.find("header")
+    if header == None:
+        continue
+
+    sectionObj = {"title": header.text.strip(), "people": []}
+
+    imgs = section.findAll("img")
+    for person, image in zip(section.findAll("div", class_="profile"), imgs):
+        personJson = {
+            "name": person.find("h1").text,
+            "contact": person.find("div", class_="contact").text,
+            "description": person.find("h2").text,
+            "img": "https://szbi-pg.hu"+image["src"]
+        }
+
+        sectionObj["people"].append(personJson)
+
+    output.append(sectionObj)
+
+print(json.dumps(output, ensure_ascii=False))
